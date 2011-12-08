3ae93b25ad4c5a8c9e988a2c5654b4fa0302b1bb
Joris Stork <joris@wintermute.eu>
2011-12-08 15:59:53 +0100

    oo-ed the block optimiser and basic blocks. now working on suboptimisations

--- src/cfg.py
+++ src/cfg.py
@@ -5,9 +5,27 @@
 from ir import *
 
 
+
 class BasicBlock(object):
-    def __init__(self, objects):
-        self.objects = objects
+    """   """
+    def __init__(self, instructions):
+        self.instructions = instructions
+
+
+    def length(self):
+        """   """
+        return len(self.objects)
+
+
+    def set_instruction(self, index, value):
+        """   """
+        self.instructions[index] = expr
+
+
+    def get_instructions(self, index, length):
+        """   """
+        return self.instructions[index:length]
+
 
 
 class CFG(object):
--- src/unittest.py
+++ src/unittest.py
@@ -2,6 +2,7 @@ import unittest
 import flat
 from ir import *
 
+
 class TestFlatOptimizer(unittest.TestCase):
     def setUp(self):
         self.insls = [[Instr("BEQ", ["2"]), Label("1"),Label("2"),Instr("j",["3"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Label("3")]]
