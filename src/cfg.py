fe54a884bfb884d0d8b22faeced23ca62d3c866a
Jeroen Zuiddam <j.zuiddam@gmail.com>
2011-12-09 13:00:29 +0100

    jaja

--- src/cfg.py
+++ src/cfg.py
@@ -9,8 +9,9 @@ from ir import *
 class BasicBlock(object):
     """   """
 
-    def __init__(self, instructions):
+    def __init__(self, instructions=[]):
         self.instructions = instructions
+        self.next = []
 
 
     def __getitem__(self, index):
@@ -25,25 +26,46 @@ class BasicBlock(object):
         self.instructions[index] = value
 
 
-    def len(self):
+    def __len__(self):
         """   """
         return len(self.instructions)
 
+    def append(self, value):
+        self.instructions.append(value)
 
-    def set_instruction(self, index, value):
-        """   """
-        self.instructions[index] = expr
-
-
-    def get_instructions(self, index, length):
-        """   """
-        return self.instructions[index:length]
-
+    def __str__(self):
+        return str(self.instructions)
+    
 
 
 class CFG(object):
-    def __init__(self, object_list):
-        pass
+    def __init__(self, flat_ir):
+        self.blocks = []
+        self.load_flat(flat_ir)
+        
+    def load_flat(self, flat_ir):
 
-    def cfg_to_list(self):
-        pass
+        labels = {}
+        block = BasicBlock()
+        self.blocks.append(block)
+        for expr in flat_ir:
+            if type(expr) == Instr and expr.instr in control_instructions:
+                block.append(expr)
+                
+                block = BasicBlock()
+                self.blocks.append(block)
+            elif type(expr) == Label:
+                
+                block = BasicBlock([expr])
+                self.blocks.append(block)
+            else:
+                block.append(expr)
+    
+    def cfg_to_flat(self):
+        reduce( lambda x,y: x+y,  [list(block) for block in self.blocks], [])
+
+        
+
+if __name__ == '__main__':
+     from asmyacc import parser
+     c = CFG([parser.parse(line) for line in open('../benchmarks/pi.s', 'r').readlines()])
