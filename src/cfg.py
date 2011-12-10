b9a89f1af4de1cea631d951cf7d3f91723663bad
Jeroen Zuiddam <j.zuiddam@gmail.com>
2011-12-10 12:50:17 +0100

    Comments

--- src/cfg.py
+++ src/cfg.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python
+
 #
-# Control Flow Graph
+# cfg.py
 #
+
+"""
+
+Control Flow Graph and Basic Block
+
+"""
 
 from ir import Instr, Label, control_instructions
 
 
 
 class BasicBlock(object):
-    """   """
+    """
+    Basic Block
+
+    """
 
     def __init__(self, instr = None):
         if not instr:
@@ -18,22 +29,19 @@ class BasicBlock(object):
 
 
     def __getitem__(self, index):
-        """   """
-        
         return self.instructions[index]
 
 
     def __setitem__(self, index, value):
-        """   """
-
         self.instructions[index] = value
 
 
     def __len__(self):
-        """   """
+        "Return number of instructions in this block."
         return len(self.instructions)
 
     def append(self, value):
+        "Append instruction to block."
         self.instructions.append(value)
 
     def __str__(self):
@@ -42,11 +50,21 @@ class BasicBlock(object):
 
 
 class CFG(object):
+    """
+    Control Flow Graph
+    
+    """
+    
     def __init__(self, flat_ir):
         self.blocks = []
         self.load_flat(flat_ir)
+
         
     def load_flat(self, flat_ir):
+        """
+        Load list of expression objects in Control Flow Graph.
+
+        """
 
         self.blocks.append(BasicBlock())
         for expr in flat_ir:
@@ -59,19 +77,24 @@ class CFG(object):
                 self.blocks.append(BasicBlock([expr]))
             else:
                 self.blocks[-1].append(expr)
+
     
     def cfg_to_flat(self):
-        #return reduce( lambda x,y: x+y,  [list(block) for block in self.blocks], [])
+        """
+        Convert Control Flow Graph to list of expression objects.
+        
+        """
+        
         return sum((list(block) for block in self.blocks), [])
 
         
 
 if __name__ == '__main__':
-     from asmyacc import parser
+    # test code
+    from asmyacc import parser
 
-     flat = []
-     for line in open('../benchmarks/pi.s', 'r').readlines():
-         if not line.strip(): continue
-         flat.append(parser.parse(line))
-         #print line, repr(flat[-1])
-     c = CFG(flat)
+    flat = []
+    for line in open('../benchmarks/pi.s', 'r').readlines():
+        if not line.strip(): continue
+        flat.append(parser.parse(line))
+    c = CFG(flat)
