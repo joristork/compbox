775354241c482eff0fb4fea97ad097f239ba2eab
holbewoner <luuk@noregular.com>
2011-12-26 21:07:27 +0100

    Testing works now...

--- src/test_flat.py
+++ src/test_flat.py
@@ -1,20 +1,31 @@
+#!/usr/bin/env python
 import unittest
 import flat
 from ir import *
 
 
 class TestFlatOptimizer(unittest.TestCase):
+    insls = []
     def setUp(self):
-        self.insls = [[Instr("BEQ", ["2"]), Label("1"),Label("2"),Instr("j",["3"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Instr("BEQ", ["2"]),Label("3")]]
+        self.insls = [[Instr("BEQ", ["2"]), 
+        Label("1"),
+        Label("2"),
+        Instr("j",["3"]),
+        Instr("BEQ", ["2"]),
+        Instr("BEQ", ["2"]),
+        Instr("BEQ", ["2"]),
+        Instr("BEQ", ["2"]),
+        Instr("BEQ", ["2"]),
+        Label("3")]]
         
     def test_optimize_jump(self):
         results = []
-        for insl in insls:
+        for insl in self.insls:
             results.append(flat.optimize_jump(insl))
          
         exps = ["[<Instr 'BEQ' ['3']>, <Label '3'>]"]
-         
-        for res,exp in zip(result, exps):
+        self.assertTrue(len(exps)==len(results))
+        for res,exp in zip(results, exps):
             self.assertEqual(str(res), exp)
         
         
