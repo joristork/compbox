f664f9796f72ee2884897bf0d5ad788ea449d323
holbewoner <luuk@noregular.com>
2011-12-31 17:59:47 +0530

    You've got a spell on me baby: bug squashed

--- src/dataflow.py
+++ src/dataflow.py
@@ -5,14 +5,17 @@ import parse_instr
 class Dataflow(object):
     def __init__(self, graph):
         self.graph = graph
-        #self.create_sets()
+        self.create_sets()
                  
     def create_sets(self):
         self.create_gen()    
         
     def create_gen(self):
-        #self.graph.get_block("main").print_block()
-        #for block in [self.graph.get_block("main")]:
+        """
+        Determines which instruction are in the gen set of each block
+        """
+        #self.graph.get_block("$L4").print_block()
+        #for block in [self.graph.get_block("$L4")]:
         for block in self.graph.blocks:
             # {"",[]}
             gen = {}
@@ -20,17 +23,17 @@ class Dataflow(object):
             for i,instr in enumerate(block.instructions):
                 if type(instr) == Instr:
                     kill = []
-                    if len(instr.ge*n) > 0:
+                    if len(instr.gen) > 0:
                         for reg in instr.gen: 
                             kill += self.check_regs(gen, reg)
                         for key in kill:
                             if key not in killown:
                                 killown[key] = gen[key]
-                            del gen[key]
+                            if key in gen:
+                                del gen[key]
                         gen[block.name + "_" + str(i)] = instr.gen
-            #print gen
             block.genset = gen
-            block.killown = killown
+            block.killown = killown   
             
     def create_kill(self):
         block.killset = []
@@ -38,19 +41,31 @@ class Dataflow(object):
             self.get_reach(block)
         
         for block in self.graph.blocks:
-            for target in self.graph.blocks:
-                if block.name in target.reach:
-                    kills = []
-                    for g in target.gen: 
-                        for reg in target.gen[g]:
-                            kills = check_regs(block.gen, reg)
-                    for g in target.killown: 
-                        for reg in target.killown[g]:
-                            kills += check_regs(block.gen, reg)                    
-                    kills = self.remove_duplicates(kills)
+            for targetname in block.reach:
+                target = self.graph.get_block(targetname)
+                kills = []
+                
+                
+                #for g in target.gen: 
+                #    for reg in target.gen[g]:
+                #        kills = check_regs(block.gen, reg)
+                #for g in target.killown: 
+                #    for reg in target.killown[g]:
+                #        kills += check_regs(block.gen, reg)  
+                                          
+                kills = self.remove_duplicates(kills)
+                for kill in kills:
+                    if kill not in target.killset:
+                        if kill in block.gen:
+                            target.killset[key] = block.gen[key]
+                        elif kill in block.killown:
+                            target.killset[key] = block.gen[key]                            
+                            
 
-
-    def remove_duplicates(self, l):7/-
+    def remove_duplicates(self, l):
+        """
+        Removes all duplicate values in a list (with hashable values)
+        """
         d = {}
         for x in l:
             d[x] = 1
@@ -59,13 +74,35 @@ class Dataflow(object):
     
                            
     def check_regs(self, dic, reg):
+        """
+        Checks if the register that is given, is in the gen dict of
+        a block. If
+        so, the key (instruction name) is added to a list and
+        returned.
+        """
         kill = []
+        if type(reg) == Register:
+            reg = reg.expr
         for key in dic: 
-            if reg in dic[key]:
-                kill.append(key)
+            for dicreg in dic[key]:
+                if type(dicreg) == Register:
+                    #print reg, dicreg.expr, reg==dicreg.expr
+                    if reg == dicreg.expr:
+                        kill.append(key)
+                elif type(dicreg) == str:
+                    #print reg, dicreg
+                    if reg == dicreg:
+                        kill.append(key)    
+        #print kill            
         return kill
         
     def get_reach(self,block):
+        """
+        Returns a list that contains all names of the blocks that can
+        be reached from the given block.
+        
+        Example: Graph = b1 -> b2 -> b3 Reach(b1) = [b2,b3]
+        """
         lengthold = 0
         reach = [block.name]
         lengthnew = 1
@@ -94,7 +131,7 @@ def main():
     flat = parse_instr.parse(flat)
     c = CFG(flat)
     d = Dataflow(c)
-    d.get_reach(c.get_block("1"))
+    d.get_reach(c.get_block("$L7"))
     
     
     return c
