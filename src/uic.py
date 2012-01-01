b7fd9c9c857620cef7b60bce0d552b80d6eec119
Joris Stork <joris@wintermute.eu>
2012-01-01 18:00:53 +0100

    copy propagation bugfix: now properly identifies and substitutes registers in instructions, also when combined with offset; uic expanded to be comprehensive. items in unsafe list in uic still need to be sorted into the proper lists in uic.

--- src/uic.py
+++ src/uic.py
@@ -11,55 +11,150 @@ Description:
 """
 
 
-#
-# IR objects
-#
-
-
 """
-Instructions (keys) that respectively assign to the registers in args[(values)]
+Instructions (keys) that respectively assign to the registers in
+args[(values)]
 
 """
 assign_to = {
-
-    'lw'    :   0   ,
-    'move'  :   0   ,
-    'mov.d' :   0   ,
-    'mov.s' :   0   ,
-    'sll'   :   0   ,
+    'abs.d' :   0   ,
+    'abs.s' :   0   , 
+    'add'   :   0   ,
+    'add.d' :   0   , 
     'addu'  :   0   ,
-    'l.s'   :   0   ,
-    'l.d'   :   0   ,
-    'l.a'   :   0   ,
-    'sub.u' :   0   ,
-    'l.s'   :   0   ,
-    'li'    :   0   ,
-    'add.d' :   0   ,
-    'div.d' :   0   ,
-    'div.s' :   0   ,
-    'div'   :   0   ,
-    'sub.d' :   0   ,
+    'add.s' :   0   ,
+    'and'   :   0   ,
     'cvt.d.w':  0   ,
     'cvt.d.s':  0   ,
     'cvt.s.d':  0   ,
     'cvt.s.w':  0   ,
-    'add.s' :   0   ,
-    'lb'    :   0   ,
-    'abs.d' :   0   ,
-    'sra'   :   0   ,
+    'cvt'   :   0   ,  
+    'c.s'   :   0   ,     
+    'c.d'   :   0   , 
+    'divu'  :   0   ,
+    'sub.u' :   0   ,
+    'sub.d' :   0   ,
+    'add.d' :   0   ,
+    'div.d' :   0   ,
+    'div.s' :   0   ,
+    'div'   :   0   ,
     'dlw'   :   0   ,
-    'neg.d' :   0   ,
-    'sub.s' :   0   ,
-    'srl'   :   0   ,
+    'lb'    :   0   ,
+    'l.s'   :   0   ,
+    'l.d'   :   0   ,
+    'l.a'   :   0   ,
+    'li'    :   0   ,
+    'lw'    :   0   ,
     'lbu'   :   0   ,
+    'lh'    :   0   ,
+    'lhu'   :   0   ,
+    'move'  :   0   ,
+    'mov.d' :   0   ,
+    'mov.s' :   0   ,
+    'mult'  :   0   , 
+    'mult.s':   0   , 
+    'mult.d':   0   , 
+    'multu' :   0   , 
     'neg.s' :   0   ,  
+    'neg.d' :   0   ,
+    'nor'   :   0   ,
     'or'    :   0   ,
-    'xori'  :   0  #,
-    #'mtc1'  :   1  check instruction
-    #'mfc1'  :   1  check instruction
-    #'dmfc1'  :   1  check instruction
-
+    'sra'   :   0   ,
+    'sub.s' :   0   ,
+    'subu'  :   0   ,
+    'sub'   :   0   ,
+    'srl'   :   0   ,
+    'sll'   :   0   ,
+    'sra'   :   0   ,    
+    'slt'   :   0   , 
+    'sqrt.s':   0   ,  
+    'sqrt.d':   0   , 
+    'sltu'  :   0   , 
+    'xor'   :   0   ,
+    'xori'  :   0   
     }
+
+
+""" 
+These instruction types may be subject to substituting copy references with
+original references as part of a copy propagation optimisation
+
+"""
+copy_prop_targets = [
+    'add',      #- integer add 
+    'addu',     #- integer add unsigned 
+    'add.s',    #- single-precision (SP) add
+    'add.d',    #- double-precision (DP) add
+    'and',      #- logical AND 
+    'beq',   #- branch == 0 
+    'bne',   #- branch != 0 
+    'blez',  #- branch <= 0 
+    'bgtz',  #- branch > 0 
+    'bltz',  #- branch < 0 
+    'bgez',  #- branch >= 0 
+    'bc1f',   #- Branch on floating point compare false.
+    'bc1t',   #- Branch on floating point compare true.
+    'cvt',       #- int., single, double conversion
+    'c.s',      #- SP compare
+    'c.d',      #- DP compare
+    'dsw',  #- store double word 
+    'div',      #- integer divide 
+    'divu',     #- integer divide unsigned 
+    'dlw',  #- load double word 
+    'or',       #- logical OR 
+    'nor',      #- logical NOR 
+    'srl',      #- shift right logical 
+    'sra',      #- shift right arithmetic 
+    'slt',      #- set less than 
+    'sltu',     #- set less than unsigned 
+    'sub.s',    #- SP subtract
+    'sub.d',    #- DP subtract
+    'div.s',    #- SP divide
+    'div.d',    #- DP divide
+    'abs.s',    #- SP absolute value
+    'abs.d',    #- DP absolute value
+    'neg.s',    #- SP negation
+    'neg.d',    #- DP negation
+    'cvt',      #- int., single, double conversion
+    'c.s',      #- SP compare
+    'c.d',      #- DP compare
+    'jr',    #- jump register 
+    'jalr',  #- jump and link register 
+    'lb',   #- load byte 
+    'lbu',  #- load byte unsigned 
+    'lh',   #- load half (short) 
+    'lhu',  #- load half (short) unsigned 
+    'lw',   #- load word 
+    'l.s',  #- load single-precision FP 
+    'l.d',  #- load double-precision FP 
+    'li'
+    'move', # extra
+    'mult.s',   #- SP multiply
+    'mult.d',   #- DP multiply
+    'mult',     #- integer multiply 
+    'multu',    #- integer multiply unsigned 
+    'nor',  #- logical NOR 
+    'or',   #- logical OR 
+    'sll',      #- shift left logical 
+    'srl',  #- shift right logical 
+    'sra',  #- shift right arithmetic 
+    'slt',  #- set less than 
+    'sltu', #- set less than unsigned 
+    'sub',      #- subtract 
+    'subu',     #- integer subtract unsigned 
+    'sub.s',    #- SP subtract
+    'sub.d',    #- DP subtract
+    'sqrt.s',   #- SP square root
+    'sqrt.d',   #- DP square root
+    'sb',   #- store byte 
+    'sbu',  #- store byte unsigned 
+    'sh',   #- store half (short) 
+    'shu',  #- store half (short) unsigned 
+    'sw',   #- store word 
+    's.s',  #- store single-precision FP 
+    's.d',  #- store double-precision FP
+    'xor'      #- logical XOR 
+    ]
 
 
 """ 
@@ -69,162 +164,9 @@ may be considered safe
 
 """
 copy_prop_unsafe = [
-
-    ]
-
-
-""" 
-These instruction types may be subject to substituting copy references with
-original references as part of a copy propagation optimisation
-
-"""
-copy_prop_targets = [
-    'addu',     #- integer add unsigned 
-    'sll',      #- shift left logical 
-    'add',      #- integer add 
-    'sub',      #- subtract 
-    'subu',     #- integer subtract unsigned 
-    'mult',     #- integer multiply 
-    'multu',    #- integer multiply unsigned 
-    'div',      #- integer divide 
-    'divu',     #- integer divide unsigned 
-    'and',      #- logical AND 
-    'or',       #- logical OR 
-    'xor',      #- logical XOR 
-    'nor',      #- logical NOR 
-    'srl',      #- shift right logical 
-    'sra',      #- shift right arithmetic 
-    'slt',      #- set less than 
-    'sltu',     #- set less than unsigned 
-    'add.s',    #- single-precision (SP) add
-    'add.d',    #- double-precision (DP) add
-    'sub.s',    #- SP subtract
-    'sub.d',    #- DP subtract
-    'mult.s',   #- SP multiply
-    'mult.d',   #- DP multiply
-    'div.s',    #- SP divide
-    'div.d',    #- DP divide
-    'abs.s',    #- SP absolute value
-    'abs.d',    #- DP absolute value
-    'neg.s',    #- SP negation
-    'neg.d',    #- DP negation
-    'sqrt.s',   #- SP square root
-    'sqrt.d',   #- DP square root
-    'cvt',      #- int., single, double conversion
-    'c.s',      #- SP compare
-    'c.d',      #- DP compare
-    ]
-
-
-control_instructions = [
-    'j',     #- jump 
-    'jal',   #- jump and link 
-    'jr',    #- jump register 
-    'jalr',  #- jump and link register 
-    'beq',   #- branch == 0 
-    'bne',   #- branch != 0 
-    'blez',  #- branch <= 0 
-    'bgtz',  #- branch > 0 
-    'bltz',  #- branch < 0 
-    'bgez',  #- branch >= 0 
-    'bct',   #- branch FCC TRUE 
-    'bcf',    #- branch FCC FALSE
-    'bc1f',   #- Branch on floating point compare false.
-    'bc1t',   #- Branch on floating point compare true.
-    ]
-
-loadstore_instructions = [
-    'lb',   #- load byte 
-    'lbu',  #- load byte unsigned 
-    'lh',   #- load half (short) 
-    'lhu',  #- load half (short) unsigned 
-    'lw',   #- load word 
-    'dlw',  #- load double word 
-    'l.s',  #- load single-precision FP 
-    'l.d',  #- load double-precision FP 
-    'sb',   #- store byte 
-    'sbu',  #- store byte unsigned 
-    'sh',   #- store half (short) 
-    'shu',  #- store half (short) unsigned 
-    'sw',   #- store word 
-    'dsw',  #- store double word 
-    's.s',  #- store single-precision FP 
-    's.d',  #- store double-precision FP
-    'move', # extra
-    'li'
-    ]
-
-intarithm_instructions = [
-    'add',  #- integer add 
-    'addu', #- integer add unsigned 
-    'sub',  #- subtract 
-    'subu', #- integer subtract unsigned 
-    'mult', #- integer multiply 
-    'multu',#- integer multiply unsigned 
-    'div',  #- integer divide 
-    'divu', #- integer divide unsigned 
-    'and',  #- logical AND 
-    'or',   #- logical OR 
-    'xor',  #- logical XOR 
-    'nor',  #- logical NOR 
-    'sll',  #- shift left logical 
-    'srl',  #- shift right logical 
-    'sra',  #- shift right arithmetic 
-    'slt',  #- set less than 
-    'sltu', #- set less than unsigned 
-    ]
-
-floatarithm_instructions = [
-    'add.s',    #- single-precision (SP) add
-    'add.d',    #- double-precision (DP) add
-    'sub.s',    #- SP subtract
-    'sub.d',    #- DP subtract
-    'mult.s',   #- SP multiply
-    'mult.d',   #- DP multiply
-    'div.s',    #- SP divide
-    'div.d',    #- DP divide
-    'abs.s',    #- SP absolute value
-    'abs.d',    #- DP absolute value
-    'neg.s',    #- SP negation
-    'neg.d',    #- DP negation
-    'sqrt.s',   #- SP square root
-    'sqrt.d',   #- DP square root
-    'cvt',       #- int., single, double conversion
-    'c.s',      #- SP compare
-    'c.d',      #- DP compare
-]
-
-misc_instructions = [
-    'nop',      #- no operation
-    'syscall',  #- system call
-    'break',    #- declare program error
-
-    #extra
     'mflo',
     'mtcl',
-    'la'
-]
-
-
-
-registers = [
-    r'\$zero' #zero-valued source/sink
-    r'\$at' #reserved by assembler
-    r'\$v[0-1]', #fn return result regs
-    r'\$a[0-3]', #fn argument value regs
-    r'\$t[0-7]', #temp regs, caller saved
-    r'\$s[0-7]', #saved regs, callee saved
-    r'\$t[8-9]', #temp regs, caller saved
-    r'\$k[0-1]', #reserved by OS
-    r'\$gp', #global pointer
-    r'\$sp', #stack pointer
-    r'\$s8', #saved regs, callee saved
-    r'\$ra', #return address reg
-    r'\$hi', #high result register
-    r'\$lo', #low result register
-    r'\$f([1-2][0-9]|3[0-1]|[0-9])', #floating point registers $f0 - $f31
-    r'\$fcc', #floating point condition code
-    r'\$([1-2][0-9]|3[0-1]|[0-9])', #extra registers $0-$31 (not in spec! :s)
-    r'\$fp',
+    'la',
+    'mfc1',
+    'dmfc1',
     ]
-
